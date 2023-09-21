7a213344f7d8c30c042cc3deeb9082399a020069
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-09-21 13:34:28 -0300

    me provider

--- lib/src/core/restClient/rest_client.dart
+++ lib/src/core/restClient/rest_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:dw_barbershop/src/core/restClient/interceptors/auth_interceptor.dart';
 
 final class RestClient extends DioForNative {
   RestClient()
@@ -8,9 +9,10 @@ final class RestClient extends DioForNative {
           connectTimeout: const Duration(seconds: 10),
           receiveTimeout: const Duration(seconds: 60),
         )) {
-    interceptors.addAll(
-      [LogInterceptor(requestBody: true, responseBody: true)],
-    );
+    interceptors.addAll([
+      LogInterceptor(requestBody: true, responseBody: true),
+      AuthInterceptor(),
+    ]);
   }
 
   RestClient get auth {
